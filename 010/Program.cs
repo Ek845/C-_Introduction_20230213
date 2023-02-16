08d40a1474d3fee1d1b1d242fabefcd4c6b36ce8
Kate <ekaterinakupcova1@gmail.com>
2023-02-16 15:01:36 +0300

    Lesson1 09

---
+++ 010/Program.cs
@@ -0,0 +1,10 @@
+﻿// 10. Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+
+Int32 n;
+int d0,d1,d2;
+System.Console.WriteLine("Введите целое число% ");
+n=System.Convert.ToInt32(Console.ReadLine());
+d0=n%10;
+d1=n/10%10;
+d2=n/100;
+System.Console.WriteLine($"{d0}");
