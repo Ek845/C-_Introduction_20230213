223b49477aa0ee32385fe62b1b27e2814de7bc55
Kate <ekaterinakupcova1@gmail.com>
2023-02-16 14:22:02 +0300

    Lesson1 07

---
+++ 007/Program.cs
@@ -0,0 +1,15 @@
+﻿//  7. Выяснить, является ли число четным.
+ Int32 N2;
+ bool odd;
+ System.Console.WriteLine("Введите число для проверки на четность");
+ N2=System.Convert.ToInt32(Console.ReadLine());
+ odd=N2%2==0;
+ if (odd==true)
+ {
+     System.Console.WriteLine($"Число {N2} четное");
+ }
+ 
+ else 
+ {
+     System.Console.WriteLine($"Число {N2} нечетное");
+ }
