5d5b2ae0b572daad469d981beace599fee082d99
Kate <ekaterinakupcova1@gmail.com>
2023-02-16 14:12:03 +0300

    Lesson1 06

---
+++ 006/Program.cs
@@ -0,0 +1,9 @@
+﻿// 6. Написать программу вычисления значения функции y = sin(a). (Класс Math). 
+
+Double y,a;
+
+System.Console.WriteLine("Введите число а: ");
+a=Convert.ToDouble(Console.ReadLine());
+
+y = Math.Sin(a);
+System.Console.WriteLine($"Значение функции y={y}");
