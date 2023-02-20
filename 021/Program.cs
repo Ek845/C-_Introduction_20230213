3e64ddf7ade7e680cd0570619cc86002531e77ef
Kate <ekaterinakupcova1@gmail.com>
2023-02-20 16:53:10 +0300

    Lesson2 02

---
+++ 021/Program.cs
@@ -0,0 +1,9 @@
+﻿// 21. Дано число, обозначающее день недели. Выяснить, является ли номер дня недели выходным.
+
+int day=3;
+
+if (day==6 || day==7) System.Console.WriteLine("Выходной");
+else
+    if (day>=1 && day<=5) System.Console.WriteLine("Рабочий день");
+        else 
+        System.Console.WriteLine("Не день недели");
