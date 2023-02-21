16d71b24ef9349a57eddfc4668cd2e2df25434f1
Kate <ekaterinakupcova1@gmail.com>
2023-02-21 16:42:34 +0300

    Lesson3 2

---
+++ 036/Program.cs
@@ -0,0 +1,16 @@
+﻿// 36. Написать программу вычисления произведения чисел от 1 до N
+
+int CompNumbers(int N)
+{int i=0;
+int comp=1;
+    while (i!=N)
+    {
+        i++;
+        comp=comp*i;
+    }
+    return comp;
+}
+int N;
+System.Console.WriteLine("Введите число N");
+N=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(CompNumbers(N));
